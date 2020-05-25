71474e8f03279a7c1cad253f02300dd63bfcf4cf
Brian Takita <brian.takita@gmail.com>
2020-05-24 20:44:02 -0700

    - esm

--- bin/sam-build.js
+++ bin/sam-build.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
-require = require('esm')(module)
-const {
+import {
 	run__cli,
 	cli__sam_build,
-} = require('@ctx-core/aws-cli')
+} from '@ctx-core/aws-cli'
 run__cli(async () => {
 	const a1__arg = process.argv.slice(2)
 	return cli__sam_build(a1__arg, {
--- bin/sam-local-start-api.js
+++ bin/sam-local-start-api.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
-require = require('esm')(module)
-const {
+import {
 	run__cli,
 	cli__sam_local_start_api,
-} = require('@ctx-core/aws-cli')
+} from '@ctx-core/aws-cli'
 run__cli(async () => {
 	const a1__arg = process.argv.slice(2)
 	return cli__sam_local_start_api(a1__arg, {
